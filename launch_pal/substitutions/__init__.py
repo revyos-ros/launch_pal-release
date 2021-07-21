b1c61a4ef46a7946c3ae7facc650644bcc4a45a9
Victor Lopez <victor.lopez@pal-robotics.com>
2021-07-21 10:48:17 +0200

    Rebase from 'upstream'

--- launch_pal/substitutions/__init__.py
+++ launch_pal/substitutions/__init__.py
@@ -12,14 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from . import arg_utils
-from . import include_utils
-from . import param_utils
-from . import substitutions
+from .load_file import LoadFile
 
 __all__ = [
-    'arg_utils',
-    'include_utils',
-    'param_utils',
-    'substitutions',
+    'LoadFile',
 ]
--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+from setuptools import find_packages
 from setuptools import setup
 
 package_name = 'launch_pal'
@@ -5,7 +6,7 @@ package_name = 'launch_pal'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
+    packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
@@ -13,10 +14,10 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='user',
+    maintainer='Victor Lopez Boya',
     maintainer_email='victor.lopez@pal-robotics.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='Launch utilities needed by PAL Robotics software',
+    license='Apache License, Version 2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
