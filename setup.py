c7e7295c26d7986c712d20696980fcf190fc1cbc
Noel Jimenez <noel.jimenez@pal-robotics.com>
2023-04-04 08:57:50 +0000

    Rebase from 'release/humble/launch_pal'

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ package_name = 'launch_pal'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.0.7',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
