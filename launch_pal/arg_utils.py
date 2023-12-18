b443e3a525d16f841a538e6cc36aaa7d647c9d3c
Noel Jimenez <noel.jimenez@pal-robotics.com>
2023-12-18 10:14:41 +0000

    Rebase from 'rpm/humble/launch_pal'

--- launch_pal/arg_utils.py
+++ launch_pal/arg_utils.py
@@ -12,10 +12,58 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration
 from launch.utilities import perform_substitutions
+from launch.actions import DeclareLaunchArgument
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True, kw_only=True)
+class LaunchArgumentsBase:
+    """This class is a dataclass containing only DeclareLaunchArgument objects."""
+
+    def __init_subclass__(cls, **kwargs):
+        annotations = getattr(cls, '__annotations__', {})
+        for attr, type_ in annotations.items():
+            if not issubclass(type_, DeclareLaunchArgument):
+                raise TypeError(
+                    f"All attributes in dataclass {cls.__name__} must have type \
+                          DeclareLaunchArgument")
+
+    def add_to_launch_description(self, launch_description: LaunchDescription):
+        """
+        Load a yaml configuration file given by the robot name.
+
+        Parameters
+        ----------
+        launch_description : LaunchDescription
+            The launch description that the Launch Arguments will be added to
+
+
+        """
+        annotations = getattr(self, '__annotations__', {})
+        for attr, type_ in annotations.items():
+            launch_description.add_action(getattr(self, attr))
+        return
 
 
 def read_launch_argument(arg_name, context):
+    """
+    Use in Opaque functions to read the value of a launch argument.
+
+    Parameters
+    ----------
+    arg_name : String
+        Name of the launch argument
+    context : LaunchContext
+        The launch context
+
+    Returns
+    -------
+    value : String
+        The value of the launch argument
+
+    """
     return perform_substitutions(context,
                                  [LaunchConfiguration(arg_name)])
